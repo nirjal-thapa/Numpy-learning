7ae890641b0751b4fdb208ee1901a3f1f96453d6
Nirjal Thapa <nirjalthapa5@gmail.com>
2026-03-19 22:01:41 +0545

    2D_array

---
+++ array folder/2d_array.py
@@ -0,0 +1,7 @@
+#2) two-dimensional array
+#It's like a table where there will be rows and coloum
+import numpy as np
+arr_2d=np.array([[1,2,3],
+                 [4,5,6],
+                 [6,7,8]])
+print(arr_2d)
