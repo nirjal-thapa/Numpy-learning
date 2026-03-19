ff7f456dd8645045e1bf93a3ce716c9c63c827da
Nirjal Thapa <nirjalthapa5@gmail.com>
2026-03-19 22:03:34 +0545

    3D_array

---
+++ array folder/3d_array.py
@@ -0,0 +1,8 @@
+#3)multi-dimensional array
+#It is a array where there is more than two dimensional (eg:3d,4d,5d)
+#matrix
+import numpy as np
+
+matrix=np.array([[2,4,6]
+                 [8,9,10]])
+print(matrix)
